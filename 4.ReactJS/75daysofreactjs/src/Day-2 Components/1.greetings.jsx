468a60fabed06ce47b2a4e055ce1c8fc7ba6ba4a
xDAnkit <xDAnkitjain@gmail.com>
2024-01-05 21:28:42 +0530

    Day-2 Components

---
+++ 4.ReactJS/75daysofreactjs/src/Day-2 Components/1.greetings.jsx
@@ -0,0 +1,35 @@
+// Normal Function
+/* function Greetings(props) {
+  console.log("Props: ", props);
+  return (
+    <div>
+      <p style={{ color: props.color || "pink" }}>
+        Welcome {props.uName} from {props.company || "TBD"}
+      </p>
+    </div>
+  );
+} */
+
+const Greetings = (props) => {
+  console.log("Props: ", props);
+
+  const showAlert = () => {
+    alert(`Welcome ${props.uName}`);
+  };
+
+  return (
+    <div>
+      <p style={{ color: props.color }}>
+        Welcome {props.uName} from {props.company}
+      </p>
+      <button onClick={showAlert}>Show Alert</button>
+    </div>
+  );
+};
+
+Greetings.defaultProps = {
+  color: "brown",
+  company: "NA",
+};
+
+export default Greetings;
