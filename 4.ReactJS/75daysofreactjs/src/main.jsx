a3be535e3b65b01016e39a7582c6866884c1aaa8
xDAnkit <xDAnkitjain@gmail.com>
2024-01-16 22:35:23 +0530

    useState Part-1

--- 4.ReactJS/75daysofreactjs/src/main.jsx
+++ 4.ReactJS/75daysofreactjs/src/main.jsx
@@ -10,11 +10,21 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 //import Mounting from "./Day-5 - Lifecycle/1.Mounting.jsx";
 //import Updating from "./Day-5 - Lifecycle/2.Updating.jsx";
-import ParentComponent from "./Day-5 - Lifecycle/4.ParentCounter.jsx";
+//import ParentComponent from "./Day-5 - Lifecycle/4.ParentCounter.jsx";
+import CounterFC from "./Day-6 useState/1.counter.jsx";
+import Greetings from "./Day-6 useState/2.greetings.jsx";
+import ToggleUI from "./Day-6 useState/3.toggleUi.jsx";
+import ProfileObj from "./Day-6 useState/4.profile-obj.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <ParentComponent />
+    <CounterFC />
+    <hr />
+    <Greetings />
+    <hr />
+    <ToggleUI />
+    <hr />
+    <ProfileObj />
     <ToastContainer />
   </>
 );
